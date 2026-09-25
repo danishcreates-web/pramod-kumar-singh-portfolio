5fe6e67709b6e4daace543997bb18e9985ed7e31
Danish <dyesh02@gmail.com>
2026-09-25 22:22:48 +0530

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="pks-v1";const A=["/","/index.html","/pramod-kumar-singh.jpg","/manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
